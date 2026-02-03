179a9c4708f1a84c42e41ade1c106c915e003dc7
sravya1710 <sravyagwarty@gmail.com>
2026-02-03 18:56:32 -0500

    Initial commit

---
+++ janus-inventory-service/src/main/java/com/janus/inventory/JanusInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.janus.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanusInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ janus-inventory-service/src/test/java/com/janus/inventory/JanusInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janus.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
